f6e944e8b3f256eb5474f721a65ceab889253f8e
Hoangqv92 <hoangqv92@gmail.com>
2019-09-17 10:46:41 -0700

    'implemented static VideoPlayer and List'

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,15 +2,15 @@ import VideoListEntry from './videoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
-    {props.exampleVideoData.map((item) =>
-      <VideoListEntry url={item.snippet.thumbnails.defaults} title = {item.snippet.thumbnails.defaults} description = {item.snippet.thumbnails.defaults} />)}
+    {props.videos.map((video) =>
+      <VideoListEntry video={video} key={video.etag}/>)}
   </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  exampleVideoData: React.PropTypes.array.isRequired
+  videos: React.PropTypes.array.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
